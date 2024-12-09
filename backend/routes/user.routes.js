a66055e8eb6411a61e122aa407ddaeb4057f700b
vaghshiva <shivavagh52@gmail.com>
2024-12-09 14:53:54 +0530

    user login

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -11,5 +11,12 @@ router.post('/register',[
 userController.registerUser
 )
 
+router.post('/login',[
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({min:6}).withMessage('Password at least 6 characters long'),
+],
+ userController.loginUser
+)
+
 
 module.exports=router;
