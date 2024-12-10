f565a1a2c94b73f8dda1201abfaf174e638fb4d4
vaghshiva <shivavagh52@gmail.com>
2024-12-10 18:16:06 +0530

    captain completed

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,6 +1,8 @@
 const userModel=require('../models/user.model');
 const bcrypt=require('bcrypt');
 const jwt=require('jsonwebtoken');
+const blackListTokenModel=require('../models/blacklistToken.model');
+const captainModel = require('../models/captain.model');
 
 module.exports.authUser=async(req,res,next)=>{
     const token=req.cookies.token || req.headers.authorization?.split(' ')[ 1 ];
@@ -8,7 +10,7 @@ module.exports.authUser=async(req,res,next)=>{
         return res.status(401).json({message: 'Unauthorized'});
     }
 
-    const isBlacklisted = await userModel.findOne({token: token});
+    const isBlacklisted = await blackListTokenModel.findOne({token: token});
     if(isBlacklisted){
         return res.status(401).json({message:'Unauthorized'});
     }
@@ -20,6 +22,29 @@ module.exports.authUser=async(req,res,next)=>{
         req.user=user;
 
         return next();
+    }
+    catch(errors){
+        return res.status(401).json({message: 'Unauthorized'}); 
+    }
+}
+
+module.exports.authCaptain=async(req,res,next)=>{
+    const token=req.cookies.token || req.headers.authorization?.split(' ')[ 1 ];
+    if(!token){
+        return res.status(401).json({message:"Unathorized"});
+    }
+
+   
+    const isBlacklisted = await blackListTokenModel.findOne({token: token});
+    if(isBlacklisted){
+        return res.status(401).json({message:'Unauthorized'});
+    }
+
+    try{
+        const decoded=jwt.verify(token,process.env.JWT_SECRET);
+        const captain=await captainModel.findById(decoded._id);
+        req.captain=captain;
+        return next()
     }
     catch(errors){
         return res.status(401).json({message: 'Unauthorized'}); 
